6220ee959b30d65e542df456b98df4aad19f6b96
Feelan <panfilovich1@gmail.com>
2020-12-26 06:24:20 +0300

    Создание файла main.py

--- main.py
+++ main.py
@@ -5,8 +5,29 @@ class Critter(object):
         self.name = name
         self.hunger = hungre
         self.boredom = boredom
+
     # функция имитации времени
     def __pass_time(self):
         self.boredom += 1
         self.hunger += 1
 
+
+    # функция которая возвращает настроение питомца
+    @property
+    def mood(self):
+        happy = self.hunger + self.boredom
+        if happy < 5:
+            m = "отлично"
+        elif 5 <= happy <= 10:
+            m = "неплохо"
+        elif 11 <= happy <= 15:
+            m = "плохо"
+        elif 16 <= happy <= 20
+            m = "ужасно"
+        return m
+
+    def talk(self):
+        print("Привет! Меня зовут " + self.name + ". Сейчас я себя чувствую себя " + self.mood + ".")
+        self.__pass_time()
+
+    def feeding(self):
